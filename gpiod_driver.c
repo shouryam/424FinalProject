c6627b110f15766a45713094273c320a0dadb8d0
shouryam <shouryamunjal@gmail.com>
2022-11-30 17:47:19 -0600

    driver

---
+++ gpiod_driver.c
@@ -0,0 +1,102 @@
+#include <linux/module.h>
+#include <linux/of_device.h>
+#include <linux/kernel.h>
+#include <linux/gpio/consumer.h>
+#include <linux/platform_device.h>
+#include <linux/init.h>
+#include <linux/gpio.h>
+#include <linux/interrupt.h>
+#include <time.h>
+
+//Inturupt handler number
+int irq_number;
+
+// //Refers to LED pin P9-27
+// struct gpio_desc *led;
+
+//refers to button pin P8_14
+struct gpio_desc *button;
+
+static clock_t last_time = NULL;
+
+/**
+ * Interrupt service routine is called, when interrupt is triggered
+ * Got this code from https://github.com/Johannes4Linux/Linux_Driver_Tutorial/blob/main/11_gpio_irq/gpio_irq.c 
+ */
+static irq_handler_t gpio_irq_handler(unsigned int irq, void *dev_id, struct pt_regs *regs) {
+	printk("In Inturrput handler, should be tirggered by button in pin P8_3\n");
+	//gpiod_set_value(led, (gpiod_get_value(led) + 1) %2);
+	clock_t new_time = clock();
+	if last_time == NULL {
+		last_time = new_time;
+	} else {
+		clock_t current = clock();
+		clock_t diff = current - before;
+		last_time = current;
+        int millseconds = diff * 1000/CLOCKS_PER_SEC;
+		printk("Milliseconds: %d", millseconds);
+		if (millseconds > 1){
+			FILE *fptr;
+			fptr = fopen("speed.txt", "w");
+			fprintf(fptr, "%d", millseconds);
+			fclose(fptr);
+		}	
+	}
+
+	return (irq_handler_t) IRQ_HANDLED; 
+}
+
+// probe function, takes in the platform device that allows us to get the references to the pins
+static int led_probe(struct platform_device *pdev)
+{
+	printk("Custom GPIOD_Driver.c inserted into debian\n");
+    //Get the pins based on what we called them in the device tree file:  "name"-gpios
+	//led = devm_gpiod_get(&pdev->dev, "led", GPIOD_OUT_LOW);
+	button = devm_gpiod_get(&pdev->dev, "button", GPIOD_IN);
+
+    //Set waiting period before next input
+	gpiod_set_debounce(button, 1000000);
+
+    //Pair the button pin with an irq number
+	irq_number = gpiod_to_irq(button);
+
+    //Pair the number with the function that handles it
+	if(request_irq(irq_number, (irq_handler_t) gpio_irq_handler, IRQF_TRIGGER_RISING, "my_gpio_irq", NULL) != 0){
+		printk("Error!\nCan not request interrupt nr.: %d\n", irq_number);
+		free_irq(irq_number, NULL);
+		return -1;
+	}
+	return 0;
+}
+
+// remove function ,takes in the platform device that allows us to get the references to the pins
+static int led_remove(struct platform_device *pdev)
+{
+	printk("Custom gpiod_driver module has been removed and irq has been freed\n");
+	irq_number = gpiod_to_irq(button);
+	//TBH not sure what goes in the second argument
+    free_irq(irq_number, NULL);
+	return 0;
+}
+
+static struct of_device_id matchy_match[] = {
+	{.compatible = "gpiod_driver"},
+	{/* leave alone - keep this here (end node) */},
+};
+
+// platform driver object
+static struct platform_driver adam_driver = {
+	.probe  = led_probe,
+	.remove  = led_remove,
+	.driver  = {
+       		.name  = "The Rock: this name doesn't even matter",
+       		.owner = THIS_MODULE,
+       		.of_match_table = matchy_match,
+	},
+};
+
+module_platform_driver(adam_driver);
+MODULE_DESCRIPTION("424\'s finest");
+MODULE_AUTHOR("GOAT");
+MODULE_LICENSE("GPL v2");
+MODULE_ALIAS("platform:adam_driver");
